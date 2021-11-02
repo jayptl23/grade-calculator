8e685c018f75bfb2cdd24c1540725e5454490134
jayptl23 <jaypatel2398@gmail.com>
2021-11-01 23:08:48 -0400

    add utils to calculate final grade

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import {useState} from 'react'
 import AssessmentItem from './components/AssessmentItem'
 import {Assessments} from './definitions'
 import {SAMPLE_ASSESSMENTS} from './mock-data'
+import {getFinalGrade, isWeightSumOneHundred} from './utils'
 
 const initialState = SAMPLE_ASSESSMENTS
 const App = () => {
 	const [assessments, setAssessments] = useState<Assessments>(initialState)
+
+	const handleClick = () => {
+		console.log(getFinalGrade(assessments))
+	}
+	const calculateGrade = isWeightSumOneHundred(assessments) ? <button onClick={handleClick}>Calculate Final Grade</button> : null
 	return (
 		<div>
 			{assessments.map(assessment => (
 				<AssessmentItem assessment={assessment} />
 			))}
+			{calculateGrade}
 		</div>
 	)
 }
---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+import { Assessments, IAssessment } from "./definitions";
+
+// Utility functions
+const sumWeights = (assessments: Assessments): number => {
+    const reducer = (previousSum: number, assessment: IAssessment) => previousSum + assessment.weight
+    const sum = assessments.reduce(reducer, 0)
+    return sum
+}
+
+const weightAsDecimal = (weight: number): number => weight / 100
+
+const getGrade = (score: number, total: number): number => score / total
+
+export const getFinalGrade = (assessments: Assessments) => {
+    const reducer = (previousSum: number, assessment: IAssessment) => {
+        const {weight, score, total } = assessment
+        let decimalWeight = weightAsDecimal(weight)
+        let grade = getGrade(score, total)
+
+        return previousSum + decimalWeight * grade
+    }
+
+    const sum = assessments.reduce(reducer, 0)
+    return sum * 100
+}
+
+// Used by components
+export const isWeightSumOneHundred = (assessments: Assessments): boolean => {
+    return sumWeights(assessments) === 100
+}
